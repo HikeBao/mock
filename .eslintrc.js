99b2acf267c63220c82b09fa02bfd29b9662f339
hikebao <hikebao@163.com>
2023-06-02 15:45:48 +0800

    update: 添加eslint校验

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    tsconfigRootDir: "."
+  },
+
+  rules: {
+    "no-console": "warn"
+  }
+};
