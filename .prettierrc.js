9ab311a03353e53903249fcda23c88e190242a1a
hikebao <hikebao@163.com>
2023-06-05 17:55:42 +0800

    style(code): 提取prettier到config，美化代码

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,34 +1,3 @@
 module.exports = {
-  // 缩进字节数
-  tabWidth: 2,
-
-  // 使用单引号代替双引号
-  singleQuote: false,
-
-  // 句尾添加分号
-  semi: true,
-
-  /**
-   * 箭头函数参数只有一个是否要有小括号
-   * avoid: x => x;
-   * always: 总有括号
-   */
-  arrowParens: "always",
-
-  /**
-   * false: <div
-   *            className=""
-   *            style={{}}
-   *         >
-   *  true: <div
-   *            className=""
-   *            style={{}}>
-   */
-  bracketSameLine: true,
-
-  // 在对象或数组最后一个元素后面是否加逗号
-  trailingComma: "none",
-
-  // 超过最大值换行
-  printWidth: 160
+  ...require("./config/base.prettier")
 };
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [0.0.2](https://github.com/HikeBao/mock/compare/v0.1.2...v0.0.2) (2023-06-05)
+
+
+### ♻️ Code Refactoring | 代码重构
+
+* **project:** 项目整体重构，变可维护性更好，扩展性更高的项目 ([e4c3404](https://github.com/HikeBao/mock/commit/e4c34043661b45e17c123601f6ee61f4b7fa6c7c))
+
 ### [0.1.2](https://github.com/HikeBao/mock/compare/v0.1.1...v0.1.2) (2023-06-05)
 
 
